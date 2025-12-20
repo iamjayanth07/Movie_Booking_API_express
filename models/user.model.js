778f59a87d9ab51eb6c4888419226b3c4ad3e249
iamjayanth07 <jayanth2760naik@gmail.com>
2025-12-20 09:41:34 +0530

    added input password comprision logic

--- models/user.model.js
+++ models/user.model.js
@@ -62,6 +62,19 @@ userSchema.pre('save', async function (next) {
 });
 
 
+/**
+ * This is going to be an instance method for user, to compare a password
+ * with the stored encrypted password
+ * @param plainPassword -> input password given by user in sign in request
+ * @returns boolean denoting whether passwords are same or not ?
+ */
+userSchema.methods.isValidPassword = async function (plainPassword) {
+    const currentUser = this;
+    const compare = await bcrypt.compare(plainPassword, currentUser.password);
+    return compare;
+}
+
+
 
 const User=mongoose.model('User',userSchema);
 module.exports=User;
